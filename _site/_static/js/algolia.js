94c981b097cedaae245a8b0c757e3173300ce1b0
Jenkins <dev.null@visionappster.com>
2021-03-15 16:56:25 +0200

    Update site

--- _site/_static/js/algolia.js
+++ _site/_static/js/algolia.js
@@ -1,7 +1,7 @@
 docsearch({
-  appId: 'MNFG6E4KCT',
-  apiKey: 'ab1dd5e10c75cf48a5b36bde9bc3d44f',
-  indexName: 'visionappster',
+  appId: 'AOWZ909P6D',
+  apiKey: '57c6eeb255d542f3ae88b288ea12928e',
+  indexName: 'doc-3-0-beta5',
   // Replace inputSelector with a CSS selector
   // matching your search input
   inputSelector: '#rtd-search-form input[type=text]',
@@ -9,6 +9,17 @@ docsearch({
   debug: false,
   algoliaOptions: {
     hitsPerPage: 8,
+  },
+  transformData: hits => {
+    const match = document.URL.match(/https?:\/\/[^/]*(\/[^/]*)/);
+    const root = match ? match[1] : '';
+    for (const hit of hits) {
+      if (typeof hit.url === 'string') {
+        // Make relative to the root of this site
+        hit.url = hit.url.replace(/https?:\/\/[^/]*/, root);
+      }
+    }
+    return hits;
   }
 });
 
